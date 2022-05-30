34503b4c297c35af4e0caff80fd94788f92fce39
Tyrrell <Tbrewster0612@gmail.com>
2022-05-30 17:57:14 -0400

    d

--- get_api.js
+++ get_api.js
@@ -7,18 +7,6 @@ function updateVisitCount() {
     fetch(countUrl)
         .then(res => res.json())
         .then(res => {
-        countElement.innerHTML = res.visits;
-    });
-}
-
-updateTemp();
-
-function updateTemp() {
-    fetch(tempUrl)
-        .then(res => res.json())
-        .then(res => {
-// jsonpath doesn't work. quick and dirty fix now, proper fix TBD
-//        tempElement.innerHTML = res.data.result[?(@.metric.job=='weatherpi')].value[1];
-        tempElement.innerHTML = res.data.result[0].value[1];
+        countElement.innerHTML = res.body;
     });
 }
