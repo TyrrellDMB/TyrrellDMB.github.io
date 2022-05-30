6eccdab7416e8e6d7e87d0f5d6be1b8a0f1935cf
Tyrrell <Tbrewster0612@gmail.com>
2022-05-30 19:57:34 -0400

    sds

--- get_api.js
+++ get_api.js
@@ -4,7 +4,17 @@ const countElement = document.getElementById('count');
 updateViewCount();
 
 function updateViewCount() {
-    fetch(countUrl)
+    return  await fetch("countUrl"),
+    {
+      method: "GET",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+		'Access-Control-Allow-Headers': '*',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': '*'
+      },
+    }
         .then(res => res.json())
         .then(res => {
         countElement.innerHTML = res.stats;
