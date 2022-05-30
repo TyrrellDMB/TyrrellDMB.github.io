71482b54e6291716ccbaa50fce038d9a4cbca8b4
Tyrrell <Tbrewster0612@gmail.com>
2022-05-30 18:32:09 -0400

    update

--- get_api.js
+++ get_api.js
@@ -7,6 +7,6 @@ function updateVisitCount() {
     fetch(countUrl)
         .then(res => res.json())
         .then(res => {
-        countElement.innerHTML = res.NaN;
+        countElement.innerHTML = res.viewcount;
     });
 }
