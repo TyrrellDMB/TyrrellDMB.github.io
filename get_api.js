005add0afaf42b6d3ce3542d340409a83e36730f
Tyrrell <Tbrewster0612@gmail.com>
2022-05-30 19:04:13 -0400

    Searching for NaN #4

--- get_api.js
+++ get_api.js
@@ -7,6 +7,6 @@ function updateViewCount() {
     fetch(countUrl)
         .then(res => res.json())
         .then(res => {
-        countElement.innerHTML = res.viewcount;
+        countElement.innerHTML = parseInt(res.viewcount);
     });
 }
