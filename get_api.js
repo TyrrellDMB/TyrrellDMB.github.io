53b3127d32f3a16d2e9769969e98d569dc52205f
Tyrrell <Tbrewster0612@gmail.com>
2022-05-30 19:33:10 -0400

    Updates

--- get_api.js
+++ get_api.js
@@ -1,24 +1,12 @@
-async function getvisits(){
-    return  await fetch("https://igf5ctozsa.execute-api.us-east-1.amazonaws.com/default/updateViews",
-    {
-      method: "GET",
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-		'Access-Control-Allow-Headers': '*',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': '*'
-      },
-    })
-    .then((response) => response.json())
-    .then((responseData) => {
-      console.log(responseData);
-      return responseData;
-    })
-    .catch(error => console.warn(error));
- }
+const countUrl = 'https://igf5ctozsa.execute-api.us-east-1.amazonaws.com/default/updateViews'
+const countElement = document.getElementById('count');
 
-async function visitCount() {
-	var visits = await getvisits();
-	document.getElementById("count").innerHTML = Quantity
+updateViewCount();
+
+function updateViewCount() {
+    fetch(countUrl)
+        .then(res => res.json())
+        .then(res => {
+        countElement.innerHTML = res.body;
+    });
 }
