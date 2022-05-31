d64702c1bf619cb1b3f25632ed30053a50453e6f
Tyrrell <Tbrewster0612@gmail.com>
2022-05-30 22:06:46 -0400

    sdasad

--- get_api.js
+++ get_api.js
@@ -7,6 +7,6 @@ function updateViewCount() {
     fetch(countUrl)
         .then(res => res.json())
         .then(res => {
-        countElement.innerText = res.val;
+        countElement.innerText = res;
     });
 }
