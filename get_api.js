0e654223c692317ab1e4510cc3c265953df70ce7
Tyrrell <Tbrewster0612@gmail.com>
2022-05-30 21:06:36 -0400

    wqdqwd

--- get_api.js
+++ get_api.js
@@ -7,6 +7,6 @@ function updateViewCount() {
     fetch(countUrl)
         .then(res => res.json())
         .then(res => {
-        countElement.innerText = res.updateViews;
+        countElement.innerText = res.val;
     });
 }
