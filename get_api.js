411ec5f0715cdb6f9d246df8d2ae542ad5e093f3
Tyrrell <Tbrewster0612@gmail.com>
2022-05-30 20:07:39 -0400

    Will this work?

--- get_api.js
+++ get_api.js
@@ -7,6 +7,6 @@ function updateViewCount() {
     fetch(countUrl)
         .then(res => res.json())
         .then(res => {
-        countElement.innerHTML = res.stats;
+        countElement.innerHTML = res.Quantity;
     });
 }
