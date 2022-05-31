0e8af26ab8fd2ac122c5d1af3a8783df505401fa
Tyrrell <Tbrewster0612@gmail.com>
2022-05-30 21:00:40 -0400

    sadsa

--- get_api.js
+++ get_api.js
@@ -7,6 +7,6 @@ function updateViewCount() {
     fetch(countUrl)
         .then(res => res.json())
         .then(res => {
-        countElement.innerHTML = res.updateValues;
+        countElement.innerHTML = res.updateViews;
     });
 }
